c8d15d2e8fd6baef8a60d8067873e99ce60997a3
jean <jean_carlos104@hotmail.com>
2015-07-30 10:32:36 -0500

    second commit

--- apps/entries/admin.py
+++ apps/entries/admin.py
@@ -5,5 +5,11 @@ from .actions import make_published, export_as_excel
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
 
-	list_display = ('id', 'title', 'published')
+	list_display = ('id', 'title', 'published', 'image', 'get_url_image', 'created')
 	actions = (make_published, export_as_excel)
+
+	def get_url_image(self, entry):
+		url = entry.url_image()
+		tag = "<img src='%s' width='100px' >" % url
+		return tag
+	get_url_image.allow_tags = True
--- apps/entries/models.py
+++ apps/entries/models.py
@@ -1,11 +1,38 @@
 from django.db import models
 
-class Entry(models.Model):
+class TimeStampModel(models.Model):
+
+	created = models.DateField(auto_now_add=True)
+	update = models.DateField(auto_now=True)
+
+	class Meta:
+		abstract = True
+
+class EntryQuerySet(models.QuerySet):
+
+	def published(self):
+		return self.filter(published = True)
+
+	def not_published(self):
+		return self.filter(published = False)
+
+
+class Entry(TimeStampModel):
 
 	title = models.CharField(max_length=50)
 	content = models.TextField()
 	published = models.BooleanField(default = False)
 	image = models.ImageField(upload_to = 'entries', null=True, blank=True)
 
+	objects = EntryQuerySet.as_manager()
+
+	entries = models.Manager()
+
 	def __unicode__(self):
 		return self.title
+
+	def url_image(self):
+		if not self.image:
+			return None
+		else:
+			return "http://localhost:8000/media/%s" % self.image
